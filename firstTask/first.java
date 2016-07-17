1b183183c743a14a7a1446c6daf0a4f67474ba6a
Oleg <vovkoleg1987@mail.ru>
2016-07-17 11:18:53 +0300

    formate code

--- firstTask/first.java
+++ firstTask/first.java
@@ -1,67 +1,68 @@
-class Entry{
-public static void main(String [] args)
-{
-Calc c = new Calc(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
-}
-static class Calc{
-private int a;
-private int b;
-
-public Calc(int a, int b) {
-      this.a = a;
-      this.b = b;
-      sum(a, b);
-      div(a, b);
-      pow(a);
-      pow(b);
-      mul(a, b);
-      minus(a, b);
-  }
-
-public int sum(int a, int b){
-  int result = a+b;
-  System.out.println("SUM = " + result);
-  return result;
-}
-
-public float div(int a, int b){
-  float bFloat = Float.valueOf(b);
-  float result = a/bFloat;
-  System.out.println("DIV = " + result);
-  return result;
-}
-
-public int pow(int a){
-   int result = a * a;
-   System.out.println("POW = " + result);
-   return result;
-}
-
-public int mul(int a, int b){
-   int result = a*b;
-   System.out.println("MUL = " + result);
-   return result;
-}
-public int minus(int a, int b){
-   int result = a - b;
-   System.out.println("MINUS = " + result);
-   return result;
-}
-
-public int getA() {
-        return a;
+class Entry {
+    public static void main(String[] args) {
+        Calc c = new Calc(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
     }
 
-    public void setA(int a) {
-        this.a = a;
-    }
+    static class Calc {
+        private int a;
+        private int b;
 
-    public int getB() {
-        return b;
-    }
+        public Calc(int a, int b) {
+            this.a = a;
+            this.b = b;
+            sum(a, b);
+            div(a, b);
+            pow(a);
+            pow(b);
+            mul(a, b);
+            minus(a, b);
+        }
 
-    public void setB(int b) {
-        this.b = b;
+        public int sum(int a, int b) {
+            int result = a + b;
+            System.out.println("SUM = " + result);
+            return result;
+        }
+
+        public float div(int a, int b) {
+            float bFloat = (float) b;
+            float result = a / bFloat;
+            System.out.println("DIV = " + result);
+            return result;
+        }
+
+        public int pow(int a) {
+            int result = a * a;
+            System.out.println("POW = " + result);
+            return result;
+        }
+
+        public int mul(int a, int b) {
+            int result = a * b;
+            System.out.println("MUL = " + result);
+            return result;
+        }
+
+        public int minus(int a, int b) {
+            int result = a - b;
+            System.out.println("MINUS = " + result);
+            return result;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public int getB() {
+            return b;
+        }
+
+        public void setB(int b) {
+            this.b = b;
+        }
     }
-}
 }
